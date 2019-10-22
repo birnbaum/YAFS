bce450e633bf6ee648e5d28cfb5baf56dfdf0c01
Philipp Wiesner <wiesner@othoz.com>
2019-10-22 10:56:40 +0200

    Refactoring folder structure and removing unnecessary files

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 # encoding: utf-8
+import os
+
 from setuptools import setup, find_packages
 
-#python setup.py sdist upload
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='yafs',
     version='0.3.0',
     author='Isaac Lera, Carlos Guerrero',
     author_email='isaac.lera@uib.es, carlos.guerrero@ouib.es',
     description='Yet Another Fog Simulator for Python.',
-    long_description='\n\n'.join(
-        open(f, 'rb').read().decode('utf-8')
-        for f in ['README.txt', 'CHANGES.txt', 'AUTHORS.txt']),
+    long_description=long_description,
     url='https://yafs.readthedocs.io',
     license='MIT License',
-    #packages=find_packages(where='src',exclude=("*.tests",)),
-    packages=find_packages(where='src',exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    packages=find_packages(where='src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_dir={'': 'src'},
     include_package_data=True,
-
-    install_requires=['simpy','pandas','networkx','numpy','tqdm'],
+    install_requires=['simpy', 'pandas', 'networkx', 'numpy', 'tqdm'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
